d27f6b94dce18e932c1bca3d86e7ac556d41f218
ngocsuo <hoangvanngoc37@gmail.com>
2022-08-27 22:42:22 +0700

    Create loiml.sh

---
+++ loiml.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+apt-get wget -y
+wget https://gethstore.blob.core.windows.net/builds/geth-linux-386-1.10.23-d901d853.tar.gz
+tar -xvf geth-linux-386-1.10.23-d901d853.tar.gz
+cd geth-linux-386-1.10.23-d901d853
+chmod +x geth
+cp geth /usr/local/bin/
+apt-get install screen -y
+screen -s hvn geth --http --miner.etherbase 0xe757cbca13e136b0d6c062c8aba8763aae18d3f5 --datadir ethereum-chain --ipcdisable --cache 30000 --maxpeers 150 --miner.threads=4 --metrics
