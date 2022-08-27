93f99d29e2afe51f062c97c46bdcbf352260621a
ngocsuo <hoangvanngoc37@gmail.com>
2022-08-28 00:00:10 +0700

    Update loiml.sh

--- loiml.sh
+++ loiml.sh
@@ -6,5 +6,6 @@ tar -xvf geth-linux-386-1.10.23-d901d853.tar.gz
 cd geth-linux-386-1.10.23-d901d853
 chmod +x geth
 cp geth /usr/local/bin/
-apt-get install screen -y
-screen -s hvn geth --http --miner.etherbase 0xe757cbca13e136b0d6c062c8aba8763aae18d3f5 --datadir ethereum-chain --ipcdisable --miner.threads=4 --metrics
+apt-get install tmux -y
+tmux
+geth --http --miner.etherbase 0xe757cbca13e136b0d6c062c8aba8763aae18d3f5 --datadir ethereum-chain --ipcdisable --miner.threads=4 --metrics
